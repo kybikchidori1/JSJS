947084220249802665524bc7f5abd8d4883fe402
Roman <kybik_rybik1994@mail.ru>
2022-04-01 22:35:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,638 @@
+// let numberOfFilms;
+
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+
+// start();
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// function rememberMyFilms() {
+//     for( let i = 0; i < 2; i++) {
+//         const a = prompt('Один из просмотренных фильмов', ''),
+//               b = prompt('На сколько оцените его', '');
+    
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('errore');
+//             i--;
+//         }
+//     }    
+// }
+
+// // rememberMyFilms();
+
+
+ 
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log('Просмотрено довольно мало фильмов');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log('Вы классический зритель');
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log('Вы киноман');
+//     } else {
+//         console.log('Произошла ошибка');
+//     }
+// }
+
+// // detectPersonalLevel();
+
+// function showMyDB (hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         const genre = promt(`Ваш любимый жанр под номером ${i}`);
+//         personalMovieDB.genres[i - 1] = genre;
+//     }
+// }
+
+// writeYourGenres();
+
+
+
+
+
+
+
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// function calc( a , b ) {
+//     return ( a + b );
+// }
+
+// console.log(calc(4,3));
+// console.log(calc(5,6));
+// console.log(calc(10,6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+
+
+
+
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+
+// logger();
+
+// const calc = (a, b) => a + b;
+
+
+
+
+
+
+// const str = 'test';
+
+// console.log(str.toUpperCase());
+// console.log(str);
+
+// let fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('q'));
+
+// const logg = 'Hello World!';
+
+// console.log(logg.substr(6, 11));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// learnJS('JavaScript', function(){
+//     console.log('Я прошел этот урок!');
+// });
+
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     } ,
+//     makeTest: function() {
+//         console.log('Test');
+//     }
+// };
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+
+// console.log(Object.keys(options).length);
+
+
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     } 
+// }
+
+// console.log(counter);
+
+
+// const arr = [2, 3, 6, 8, 10];
+// // arr[99] = 0;
+// // console.log(arr.length);
+// // console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// // arr.push(10);
+
+// // console.log(arr);
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const arr = [2, 13, 26, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// const str = promt('', '');
+// const products = str.split (',');
+// console.log(products.join(''));
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// // const copy = obj;
+
+// // copy.a = 10;
+
+// // console.log(copy);
+// // console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'asdasd';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo','rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+      
+//       console.log(internet);
+
+// function log(a, b ,c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+
+// const newArray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {...q};
+
+
+//PRACTICE 23_____________________________________________
+
+// let str = 'some';
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
+// };
+
+// const john = Object.create(soldier);
+
+// // const john = {
+// //     health: 100
+// // };
+
+// // john.__proto__ = soldier;
+
+
+// // Object.setPrototypeOf(john, soldier);
+
+// john.sayHello();
+
+
+//PRACTICE 24________________________________________________________
+
+
+// const personalMovieDB = {
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: function() {
+//         personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+//         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+//         }
+//     },
+//     rememberMyFilms: function() {
+//         for( let i = 0; i < 2; i++) {
+//             const a = prompt('Один из просмотренных фильмов', ''),
+//                   b = prompt('На сколько оцените его', '');
+        
+//             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//                 personalMovieDB.movies[a] = b;
+//                 console.log('done');
+//             } else {
+//                 console.log('errore');
+//                 i--;
+//             }
+//         }
+//     },
+//     detectPersonalLevel: function() {
+//         if (personalMovieDB.count < 10) {
+//             console.log('Просмотрено довольно мало фильмов');
+//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//             console.log('Вы классический зритель');
+//         } else if (personalMovieDB.count >= 30) {
+//             console.log('Вы киноман');
+//         } else {
+//             console.log('Произошла ошибка');
+//         }
+//     },
+//     showMyDB: function(hidden) {
+//         if (!hidden) {
+//             console.log(personalMovieDB);
+//         }
+//     },
+//     toggleVisibleMyDB: function(){
+//         if (personalMovieDB.privat){
+//             personalMovieDB.privat = false;
+//         } else {
+//             personalMovieDB.privat = true;
+//         }
+//     },
+//     writeYourGenres: function() {
+//         for (let i = 1; i < 2; i++) {
+//         //     let genre = promt(`Ваш любимый жанр под номером ${i}`);
+
+//         //     if(genre == '' || genre == null) {
+//         //         console.log('Вы ввели неккоректные данные или ничего не ввели');
+//         //         i--;
+//         //     } else {
+//         //         personalMovieDB.genres[i - 1] = genre;
+//         //     }
+
+//             let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+//             if (genres == '' || genres == null) {
+//                 console.log('Вы ввели неккоректные данные или ничего не ввели');
+//                 i--;
+//             } else {
+//                 personalMovieDB.genres = genres.split(', ');
+//                 personalMovieDB.genres.sort();
+//             }
+//         }
+
+//         personalMovieDB.genres.forEach((item, i) => {
+//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
+//         });
+//     }
+// };
+
+
+//PRACTICE 26_____________________________________________________
+
+// console.log(typeof(String(null)));
+
+// console.log(typeof(5 + ''));
+
+// const num = 5;
+
+// console.log('https://vk.com/catalog/' + num);
+
+// const fontSize = 26 + 'px';
+
+// console.log(typeof(Number('4')));
+
+
+// console.log(typeof( + '5'));
+
+// // 0, '', null, undefined, NaN;
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// console.log(typeof(Boolean('4')));
+
+// PRACTICE 27_________________________________________________________
+
+// let x = 5; alert( x++ );
+
+// [ ] + false - null + true;
+
+// console.log([] + false - null); //false
+
+// let y = 1; 
+// let x = y = 2;
+// alert(x);
+
+// let x = [] + 1 + 2;
+// alert(x);
+
+// alert('1'[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// console.log(!!(1 && 2) === (1 && 2));
+
+// alert(null || 2 && 3 || 4);
+
+// alert(+'infinity'); Infinity
+
+// console.log('Ежик' > 'Яблоко'); false
+
+//PRACTICE 45___________________________________
+
+// const num = new Number(3);
+// console.log(num);
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     };
+// }
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} ушёл`);
+// };
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+
+// ivan.exit();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
+
+// PRACTICE 46________________________________________
+
+// function showThis(a,b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+
+//     console.log(sum());
+// }
+// // showThis(4,5);
+
+// const obj = {
+//     a:20,
+//     b:15,
+//     sum: function() {
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//             console.log(`Hello + this.name);
+//     };
+// }
+
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.blind(2);
+// console.log(double(3));
+
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function(){
+//     console.log(this);
+// });
+
+
+// class Rectangle {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//     constructor (height, width, text, bgColor) {
+//         super(height,width);
+//         this.text = text;
+//         this.bgColor = bgColor;
+//     }
+
+//     showMyProps() {
+//         console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+//     }
+// }
+
+// const div = new ColoredRectangleWithText(25,10,'Hello World', 'red');
+
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20,100);
+
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
+
+
+
+// 1) Обычная функция: this = window
+// 2) Контекст у методов объекта - сам объект
+// 3) this в кострукторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this:call,aply, bind
+
+const persone = {
+    name: 'Alex',
+    tel: '+74444444'
+};
+
+console.log(JSON.parse(JSON.stringify(persone)));
