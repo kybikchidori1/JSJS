e7f6fe366d30fc423616aea5d3a92821e92151d7
Roman <kybik_rybik1994@mail.ru>
2022-04-03 13:17:15 +0300

    final practice 55

--- js/script.js
+++ js/script.js
@@ -630,9 +630,59 @@
 // 3) this в кострукторах и классах - это новый экземпляр объекта
 // 4) Ручная привязка this:call,aply, bind
 
-const persone = {
-    name: 'Alex',
-    tel: '+74444444'
-};
+// const persone = {
+//     name: 'Alex',
+//     tel: '+74444444'
+// };
 
-console.log(JSON.parse(JSON.stringify(persone)));
+// console.log(JSON.parse(JSON.stringify(persone)));
+
+// __________________________PRACTICE 55____________________
+
+
+// console.log('Запрос данных...');
+
+// const req = new Promise(function(resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Подготовка данных...');
+
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+
+//         resolve(product);
+//     }, 2000);
+// });
+
+//     req.then((product) => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 product.status = 'order';
+//                 resolve(product);
+//             }, 2000);
+//         });
+//     }).then(data => {
+//         data.modify = true;
+//         return data;
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(() => {
+//         console.error('Произошла ошибка');
+//     }).finally(() => {
+//         console.log('Finally');
+//     });
+
+
+// const test = time => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), time);
+//     });
+// };
+
+// // test(1000).then(() => console.log('1000 ms'));
+// // test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
